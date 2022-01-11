5ac82fa371d1fc2109e24c1528aff3b958519e1b
Bluette1 <marylene.sawyer@gmail.com>
2022-01-12 01:49:02 +0200

    Working with data

--- tests/integration/components/rental-test.js
+++ tests/integration/components/rental-test.js
@@ -10,8 +10,6 @@ module('Integration | Component | rental', function (hooks) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    // await render(hbs`<Rental />`);
-
     this.setProperties({
       rental: {
         title: 'Grand Old Mansion',
